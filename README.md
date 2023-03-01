eaac115b2f9a6eca181b69fd28104f6c9baca899
Rowan Molony <rowan.molony@mainstreamrp.com>
2023-03-01 10:00:12 +0000

    feat: create a Django/devenv project
    Includes a failing test

---
+++ README.md
@@ -0,0 +1,9 @@
+# Reproduce exit CI on failing Python tests in devenv.sh environments
+
+I have a Django/Postgres web app that uses [devenv.sh](https://devenv.sh/) for its development environment.  It is failing in GitHub Actions even though all tests are passing, this repo will attempt to reproduce
+
+---
+
+## Installation
+
+Install [`devenv.sh`](https://devenv.sh/getting-started) & launch via `devenv up`
--- devenv.nix
+++ devenv.nix
@@ -1,26 +1,69 @@
 { pkgs, ... }:
 
-{
-  # https://devenv.sh/basics/
-  env.GREET = "devenv";
+let
+  db_user = "postgres";
+  db_host = "localhost";
+  db_port = "5432";
+  db_name = "db";
+  dj = djangoArgs: ''
+    devenv shell python manage.py collectstatic --no-input
+    && devenv shell python manage.py ${djangoArgs}
+  '';
+  
+  test = testArgs: ''
+    echo "Launching database..."
+    # Redirect logs to `devenv.log` via `> /tmp/devenv.log 2>&1`
+    # ... & run in background via `&`
+    start-db > /tmp/devenv.log 2>&1 & 
+    
+    echo "Waiting for database to launch..."
+    while ! nc -z localhost ${db_port}; do
+      sleep 0.1
+    done
 
-  # https://devenv.sh/packages/
-  packages = [ pkgs.git ];
+    # Run tests
+    python manage.py test ${testArgs}
+
+    # Kill background processes running the database & Django server
+    fuser -k ${db_port}/tcp
+  '';
+in
+{
+  packages = [ pkgs.git pkgs.postgresql_14 pkgs.python310 pkgs.poetry ];
+
+  env = {
+    PYTHON_VERSION = "3.10";
+    DATABASE_URL = "postgresql://${db_user}@${db_host}:${db_port}/${db_name}";
+    DEBUG = true;
+    STATIC_ROOT = "/tmp/static";
+  };
 
   enterShell = ''
-    hello
-    git --version
+    echo "Building poetry virtual environment..."
+    poetry env use $PYTHON_VERSION
+    poetry install
+
+    # Automatically activate virtual environment on shell entry
+    # https://pythonspeed.com/articles/activate-virtualenv-dockerfile/
+    VIRTUAL_ENV=`poetry env info --path` && export VIRTUAL_ENV 
+    export PATH="$VIRTUAL_ENV/bin:$PATH";
+
+    echo "Using versions..."
+    psql --version
+    python --version
+    pip --version
+    poetry --version
   '';
 
-  # https://devenv.sh/languages/
-  # languages.nix.enable = true;
+  services.postgres = {
+    enable = true;
+    initialScript = "CREATE USER ${db_user} SUPERUSER;";
+    initialDatabases = [ { name = db_name; } ];
+    listen_addresses = db_host;
+  };
 
-  # https://devenv.sh/scripts/
-  # scripts.hello.exec = "echo hello from $GREET";
-
-  # https://devenv.sh/pre-commit-hooks/
-  # pre-commit.hooks.shellcheck.enable = true;
-
-  # https://devenv.sh/processes/
-  # processes.ping.exec = "ping example.com";
+  scripts = {
+      run-tests.exec = test "";
+      start-db.exec = "devenv up";
+  };
 }
